2fcad69e0520b5f17dc39852ba6fffa70e8417a0
Luis Cochachi <lcochachi@gmail.com>
2022-10-12 17:43:48 -0500

    006

--- src/main/java/com/persq/springboot/app/items/controllers/ItemController.java
+++ src/main/java/com/persq/springboot/app/items/controllers/ItemController.java
@@ -1,51 +1,86 @@
 package com.persq.springboot.app.items.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.persq.springboot.app.items.models.Item;
 import com.persq.springboot.app.items.models.Producto;
 import com.persq.springboot.app.items.models.service.ItemService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 @RestController
 public class ItemController {
+private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
 	
 	@Autowired
 	@Qualifier("serviceFeign")
-//	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name="nombre", required= false) String nombre, @RequestHeader(name="token-request", required = false) String token){
-		System.out.println(nombre);
-		System.out.println(token);
+//		System.out.println(nombre);
+//		System.out.println(token);
 		return itemService.findAll();
 	}
 	
-	@HystrixCommand(fallbackMethod="metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return cbFactory.create("items")
+				.run(()-> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
+	}
+	
+	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
+	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
+	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
 	
-	public Item metodoAlternativo(Long id, Integer cantidad) {
+	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name="items")
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return CompletableFuture.supplyAsync(()-> itemService.findById(id, cantidad));
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
 		Item item = new Item();
 		Producto producto = new Producto();
 		
 		item.setCantidad(cantidad);
 		producto.setId(id);
-		producto.setNombre("Camara Sony");
+		producto.setNombre("Camara Sin Marca");
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return item;
 	}
 
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sin Marca 2");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return CompletableFuture.supplyAsync(()-> item);
+	}
 }
