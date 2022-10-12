aca617c8acb2e467c12067b0ce20148f2cb824ef
Luis Cochachi <lcochachi@gmail.com>
2022-10-12 11:32:08 -0500

    003

--- src/main/java/com/persq/springboot/app/items/controllers/ItemController.java
+++ src/main/java/com/persq/springboot/app/items/controllers/ItemController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.persq.springboot.app.items.models.Item;
+import com.persq.springboot.app.items.models.Producto;
 import com.persq.springboot.app.items.models.service.ItemService;
 
 @RestController
@@ -24,9 +26,22 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod="metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sin Marca");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
 	}
 
 }
